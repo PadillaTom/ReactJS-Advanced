897fc6fe13fff06366083ac82e2705bc4efff789
Tomas Padilla <padillatomasagustin@gmail.com>
2022-03-14 20:36:50 +0100

    Testing - Enzyme Shallow, tests

--- my-app/src/components/__tests__/App.test.js
+++ my-app/src/components/__tests__/App.test.js
@@ -1,27 +1,32 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import App from "../App";
-import { CommentBox } from "../comments";
+import { CommentBox, CommentList } from "../comments";
 
 it("Shows a CommentBox", () => {
-	// We use JSDOM Library to "imitate" a DOM, creating elements
-	const myDiv = document.createElement("div");
-	// We muont our App HTML inside this DIV.
-	ReactDOM.render(<App></App>, myDiv);
+	//
+	// === Old Way, NOT Using a Library ===
+	// // We use JSDOM Library to "imitate" a DOM, creating elements
+	// const myDiv = document.createElement("div");
+	// // We muont our App HTML inside this DIV.
+	// ReactDOM.render(<App></App>, myDiv);
+	// //  === Test ===
+	// // 1) See whats inside:
+	// // console.log(myDiv.innerHTML);
+	// // 2) Knowing what CommentBox shows, we can find it by it's content:
+	// // NOT GOOD APPROACH.
+	// // expect(myDiv.innerHTML).toContain("CommentBox");
+	// // === End Test ===
+	// // Unmount our App, for tests performance
+	// ReactDOM.unmountComponentAtNode(myDiv);
+	//
+	// === Using Enzyme ===$
+	const wrapped = shallow(<App></App>);
+	expect(wrapped.find(CommentBox).length).toEqual(1);
+});
 
-	//  === Test ===
-	// 1) See whats inside:
-	// console.log(myDiv.innerHTML);
-
-	// 2) Knowing what CommentBox shows, we can find it by it's content:
-    // NOT GOOD APPROACH.
-	// expect(myDiv.innerHTML).toContain("CommentBox");
-
-    // 3) To see if it has an instance of:
-
-
-	// === End Test ===
-
-	// Unmount our App, for tests performance
-	ReactDOM.unmountComponentAtNode(myDiv);
+it("Shows a CommentList", () => {
+	const wrapped = shallow(<App></App>);
+	expect(wrapped.find(CommentList).length).toEqual(1);
 });
