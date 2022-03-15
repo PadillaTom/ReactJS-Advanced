b25b3dc6bfefc7b39d0dd61b6b9b808024f2f022
Tomas Padilla <padillatomasagustin@gmail.com>
2022-03-15 11:47:02 +0100

    Functional -  AccordionApp Started

---
+++ my-app/src/components/accordion/Accordion.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Accordion = ({ myItems }) => {
+	return <div>Accordion : {myItems.length}</div>;
+};
+
+export default Accordion;
--- my-app/src/components/app/App.js
+++ my-app/src/components/app/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Accordion } from "../accordion";
+
+const items = [
+	{
+		title: "What is React?",
+		content: "asdasdasd",
+	},
+	{
+		title: "What is JSX?",
+		content: "asdasdasd",
+	},
+	{
+		title: "What is Angular?",
+		content: "asdasdasd",
+	},
+];
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<Accordion myItems={items}></Accordion>
+		</div>
+	);
 };
 
 export default App;
